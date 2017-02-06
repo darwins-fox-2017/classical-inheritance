ddc0db5f6b98c811355cb0a0f006eaaf277ac3d2
samuelphsinaga <samuelphsinaga@gmail.com>
2017-02-06 15:29:20 +0700

    clasical inherintance

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,40 @@
 "use strict"
+class Animal {
+  constructor (jenis){
+    this.jenis = jenis
+  }
+}
+
+class Amphibian extends Animal{
+  constructor (jenis, is_warm_blooded){
+    super (jenis)
+    this.is_warm_blooded = is_warm_blooded
+  }
+}
+
+class Frog extends  Amphibian{
+  constructor(jenis, is_warm_blooded, warna){
+    super (jenis, is_warm_blooded)
+    this.warna = warna
+  }
+}
+
+class SuperPowerFrog extends Frog{
+  constructor(jenis, is_warm_blooded, warna, kekuatan){
+  super (jenis, is_warm_blooded, warna)
+  this.kekuatan = kekuatan
+  }
+
+  power(){
+    this.kekuatan = "Kage bunshin no jutsu"
+    return `jenis : ${this.jenis}, Berdarah Hangat : ${this.is_warm_blooded}, Warna: ${this.warna}, kekuatan: ${this.kekuatan}`
+  }
+
+}
+
+
+var xxx = new SuperPowerFrog('Kodok Naruoto', 'Tidak','Hitam')
+console.log(xxx.power())
+
+var vvv = new SuperPowerFrog('Kodok Sasuke', 'Tidak','Putih')
+console.log(vvv.power())
