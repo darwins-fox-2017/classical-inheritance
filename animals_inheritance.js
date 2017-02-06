3235b1d757a8bc2c21405ce6ed32340f3f81c248
Triyan Arief Wibowo <triyanarief@icloud.com>
2017-02-06 15:18:53 +0700

    done

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,68 @@
 "use strict"
+
+class Animal {
+  constructor(is_warm_blooded) {
+    this.is_warm_blooded = is_warm_blooded;
+  }
+
+  is_warm_blooded() {
+    return is_warm_blooded;
+  }
+}
+
+class Primate extends Animal {
+  constructor(is_warm_blooded, num_legs) {
+    super(is_warm_blooded);
+    this.num_legs = num_legs;
+  }
+
+  getNumLegs() {
+    return this.num_legs;
+  }
+}
+
+class Amphibian extends Animal {
+  constructor(is_warm_blooded, num_legs) {
+    super(is_warm_blooded);
+    this.num_legs = num_legs;
+  }
+
+  getNumLegs() {
+    return this.num_legs;
+  }
+}
+
+class Chimpanze extends Primate {
+  constructor(is_warm_blooded, num_legs, name) {
+    super(is_warm_blooded, num_legs);
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+}
+
+class Frog extends Amphibian {
+  constructor(is_warm_blooded, num_legs, name) {
+    super(is_warm_blooded, num_legs);
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+}
+
+class SuperPowerChimpanze extends Chimpanze {
+  constructor(is_warm_blooded, num_legs, name) {
+    super(is_warm_blooded, num_legs);
+  }
+
+  smashLikeHulk() {
+    return "Destroyed Building : " + Math.floor(Math.random()*6) + 1;
+  }
+}
+
+let simpanse = new SuperPowerChimpanze(true, 2, 'simpanse');
+console.log(simpanse.smashLikeHulk())
