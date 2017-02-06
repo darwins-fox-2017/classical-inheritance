de796c573720e34a1415cc1537221c74945932c4
Danang Aji Tamtomo & Isumi <pair+danangtamtomo.isumizumi@devbootcamp.com>
2017-02-06 16:32:21 +0700

    Revisi

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -17,38 +17,32 @@ class Animal {
   }
 }
 
-class Mammals extends Animal {
-  constructor(name, species, num_legs) {
+class Fox extends Animal {
+  constructor(name, species, num_legs, superpower) {
     super(name, species, num_legs)
+    this.superpower = new SuperPowered()
     this.num_legs = 4
   }
 }
 
-let animal = new Animal('Fox', 'Darwin Fox', 4)
-console.log(animal.num_legs) // ini akan menghasilkan num_legs pada class animal = 2
-let fox = new Mammals()
-
-/**
- Ini akan menghasilkan nilai num_legs yang sama pada num_legs pada animal jika num_legs pada mamals tidak dioverride yang maka akan menghasilkan 2.
- Tapi, jika dioverride pada class mamals yang mana bernilai 4, maka num_legs akan yang dipanggil dari objek fox akan menghasilkan nilai 4
- **/
-console.log(fox.num_legs)
-
 /**
  * Release 1
  */
-
- class SuperPowered extends Mammals{
-   constructor(name, species, num_legs, power) {
-      super(name, species, num_legs, power)
+ class SuperPowered {
+   constructor() {
    }
 
-   power() {
+   sneakingPower() {
      return 'Super Power: Sneaking'
    }
  }
 
- let foxPower = new SuperPowered('Darwin', 'Fox', 4)
-
- foxPower.getAnimal()
- console.log(foxPower.power())
+let fox = new Fox('Fox', 'Darwin Fox')
+console.log(`Name : ${fox.name}`);
+console.log(`Species : ${fox.species}`);
+console.log(`Num Legs : ${fox.num_legs}`);
+console.log(`${fox.superpower.sneakingPower()}`);
+/**
+ Ini akan menghasilkan nilai num_legs yang sama pada num_legs pada animal jika num_legs pada mamals tidak dioverride yang maka akan menghasilkan 2.
+ Tapi, jika dioverride pada class mamals yang mana bernilai 4, maka num_legs akan yang dipanggil dari objek fox akan menghasilkan nilai 4
+ **/
