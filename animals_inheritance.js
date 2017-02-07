49d3129b61b0d39a506d8bd8dbe3512972c8b218
mfpebrizal <ftm.pebrizal@gmail.com>
2017-02-07 07:06:12 +0700

    Done all release

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,54 @@
 "use strict"
+
+class Animal{
+  constructor(component){
+    this.nameofAnimal = component['nameofAnimal']
+    this.isWarmBlood  = component['isWarmBlood']
+    this.numOfLegs    = component['numOflegs']
+  }
+}
+
+class Vertebrate extends Animal{
+  constructor(component){
+    super(component)
+    this.hasBackBone = true
+  }
+}
+
+class Mammal extends Vertebrate{
+  constructor(component){
+    super(component)
+    this.suckling = true
+    this.hairy = true
+  }
+}
+
+class Carnivore extends Mammal {
+  constructor(component){
+    super(component)
+    this.meatLover = true
+  }
+}
+
+class Cat extends Carnivore{
+  constructor(component){
+    super(component)
+    this.smart = true
+  }
+}
+
+class SuperPoweredCat extends Cat{
+  constructor(component){
+    super(component)
+    this.superPower = "Cuteness overload"
+  }
+}
+
+let MoonCat = {
+  'nameofAnimal' : 'Cat',
+  'isWarmBlood'  : 'Yes',
+  'numOflegs'    : 4
+}
+var superCat = new SuperPoweredCat(MoonCat)
+
+console.log(superCat)
