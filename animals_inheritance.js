d2731270c009b3fdc5372a18173d126d7f5d7e23
pisanggoreng <7ofpentacles@gmail.com>
2017-02-10 11:00:45 +0700

    clear

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,21 +1,23 @@
 "use strict"
 // parent class
 class Animal {
-  constructor(name, type, food) {
+  constructor(name, type, food, power) {
       this.name = name
       this.type = type
       this.food = food
+      this.power = new SuperPowered(name, power)
   }
 
   print() {
-    console.log(`this animal name is: ${this.name}, have type ${this.type}, and eat ${this.food} evveryday`);
+    console.log(`this animal name is: ${this.name}, have type ${this.type}, and eat ${this.food}
+      evveryday, & have ${this.power}`);
   }
 }
 
 // child class
 class Dog extends Animal {
-  constructor(name, type, food) {
-    super(name, type, food)
+  constructor(name, type, food, power) {
+    super(name, type, food, power)
   }
 
   printDog() {
@@ -24,8 +26,8 @@ class Dog extends Animal {
 }
 
 class Cat extends Animal {
-  constructor(name, type, food) {
-    super(name, type, food)
+  constructor(name, type, food, power) {
+    super(name, type, food, power )
   }
 
   printCat() {
@@ -33,19 +35,29 @@ class Cat extends Animal {
   }
 }
 
-var neng = new Cat('neng','kampung','rice + fish')
+// class SuperPowered  {
+//   constructor(name,power) {
+//     this.power = power
+//     this.name = name
+//   }
+//
+//   BeInvisible(power) {
+//     this.power = power
+//     console.log(` ${this.name} have power to ${this.power}`);
+//   }
+// }
 
-class SuperPowerDog extends Dog {
-  constructor(name, type, food, power) {
-    super(name, type, food)
+class SuperPowered  {
+  constructor(name, power) {
+    this.name = name
     this.power = power
   }
 
-  BeInvisible(power) {
-    this.power = power
-    console.log(`${this.name} have power to ${this.power}`);
+  BeInvisible() {
+    console.log(` ${this.name} have power to ${this.power}`);
   }
 }
 
-var jacky = new SuperPowerDog('jacky','mini pom','rice + chicken', 'hide when feel tired')
-jacky.BeInvisible('running');
+
+var neng = new Cat('neng','kampung','rice + fish', 'hide')
+neng.power.BeInvisible();
